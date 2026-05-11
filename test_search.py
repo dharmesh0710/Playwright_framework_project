f280f49bdf86efbae8c9a47f8af4661320825691
Dharmesh <dharmesh071089@gmail.com>
2026-05-11 18:58:19 +0530

    Completed day 3: pytest fundamentas, asserstions and markers

---
+++ test_search.py
@@ -0,0 +1,22 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+
+# Tagging this as a critical "smoke" test
+@pytest.mark.smoke
+def test_search_for_hammer(page: Page):
+    page.goto("https://practicesoftwaretesting.com/")
+    page.get_by_placeholder("Search").fill("Hammer")
+    page.get_by_role("button", name="Search").click()
+
+    expect(page.locator(".card-title").first).to_contain_text("Hammer")
+
+
+# Tagging this as a standard "regression" test
+@pytest.mark.regression
+def test_search_for_pliers(page: Page):
+    page.goto("https://practicesoftwaretesting.com/")
+    page.get_by_placeholder("Search").fill("Pliers")
+    page.get_by_role("button", name="Search").click()
+
+    expect(page.locator(".card-title").first).to_contain_text("Pliers")
